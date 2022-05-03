dc29844b17c50f72c297a2f61df3178356773382
Alecsg09 <alecsander_g@hotmail.com>
2022-05-03 11:20:16 -0300

    Atualizando classe model

---
+++ src/main/java/com/generation/sustemcity/model/SustemCity.java
@@ -0,0 +1,56 @@
+package com.generation.sustemcity.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+@Entity
+@Table (name = "tb_categoria")
+public class SustemCity {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long ID;
+	
+	@NotBlank(message = "Obrigatório")
+	@Size (min=5, max=100, message="o mínimo 5 e no máximo 100 caracteres")
+	private String categoria;
+	
+	@NotNull (message = "Obrigatório")
+	@Size (min=5, max=1000, message="o mínimo 5 e no máximo 1000 caracteres")
+	private String descricao;
+
+	public Long getID() {
+		return ID;
+	}
+
+	public void setID(Long iD) {
+		ID = iD;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+
+}
